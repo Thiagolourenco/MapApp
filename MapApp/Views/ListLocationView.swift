7c60957600a5a7d5f39bfca8ae45c02151398c7e
Thiago Lourenço <thiagolourencosaraiva123@gmail.com>
2026-03-20 14:31:20 -0300

    feat: It was make the separetion the components,  in ListLocation view to show list the location and extension to header

---
+++ MapApp/Views/ListLocationView.swift
@@ -0,0 +1,18 @@
+//
+//  ListLocationView.swift
+//  MapApp
+//
+//  Created by Thiago Lourenço on 20/03/26.
+//
+
+import SwiftUI
+
+struct ListLocationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListLocationView()
+}
