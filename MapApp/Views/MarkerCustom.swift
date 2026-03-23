982a639172bdfb6c3b2d24e0efde6c24b5a3f3f5
Thiago Lourenço <thiagolourencosaraiva123@gmail.com>
2026-03-23 14:18:11 -0300

    feat: Added a marker in map

---
+++ MapApp/Views/MarkerCustom.swift
@@ -0,0 +1,18 @@
+//
+//  MarkerCustom.swift
+//  MapApp
+//
+//  Created by Thiago Lourenço on 23/03/26.
+//
+
+import SwiftUI
+
+struct MarkerCustom: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MarkerCustom()
+}
